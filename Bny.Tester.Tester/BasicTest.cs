ca89c573da934e7b5fabb5c897fa91819ab19e95
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-11-25 14:33:42 +0100

    Refactor BasicTest.Test_Tester test functions

--- Bny.Tester.Tester/BasicTest.cs
+++ Bny.Tester.Tester/BasicTest.cs
@@ -14,29 +14,29 @@ class BasicTest
         Tester t = new(nameof(Test_Tester));
         t.Out = new StringWriter();
 
-        a.Assert(t.Test(Test_Tester_Function1));
-        a.Assert(!t.Test(Test_Tester_Function2));
-        a.Assert(!t.Test(Test_Tester_Function3));
+        a.Assert(t.Test(Function1));
+        a.Assert(!t.Test(Function2));
+        a.Assert(!t.Test(Function3));
         a.Assert(t.Count() == 6);
         a.Assert(t.Where(p => p.Success).Count() == 3);
         a.Assert(t.Where(p => p.Call == "3 < 5").Count() == 2);
-    }
 
-    public static void Test_Tester_Function1(Asserter a)
-    {
-        a.Assert(3 < 5);
-        a.Assert(4 < 5);
-    }
+        static void Function1(Asserter a)
+        {
+            a.Assert(3 < 5);
+            a.Assert(4 < 5);
+        }
 
-    public static void Test_Tester_Function2(Asserter a)
-    {
-        a.Assert(3 < 5);
-        a.Assert(9 < 5);
-    }
+        static void Function2(Asserter a)
+        {
+            a.Assert(3 < 5);
+            a.Assert(9 < 5);
+        }
 
-    public static void Test_Tester_Function3(Asserter a)
-    {
-        a.Assert(3 > 5);
-        a.Assert(9 < 5);
+        static void Function3(Asserter a)
+        {
+            a.Assert(3 > 5);
+            a.Assert(9 < 5);
+        }
     }
 }
