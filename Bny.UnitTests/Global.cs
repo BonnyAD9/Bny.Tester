3f66dfe9f3fae9733766b8308ea5aabd4384ad7d
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-11-26 21:14:38 +0100

    Supress xml comment warning in Global.cs

--- Bny.UnitTests/Global.cs
+++ Bny.UnitTests/Global.cs
@@ -1,4 +1,6 @@
-﻿/**
+﻿#pragma warning disable CS1587 // XML comment is not placed on a valid language element
+/**
  * @mainpage
  * Library for unit testing in C#.
  */
+#pragma warning restore CS1587 // XML comment is not placed on a valid language element
